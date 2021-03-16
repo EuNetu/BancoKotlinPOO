4b2729f4ad83aee82e72944e2178e7992132aa18
Neto25 <euneto2501@gmail.com>
2021-03-16 03:21:52 -0300

    criação classe funcionario com seu teste

--- src/main/kotlin/one/digitalinnovation/bancokotlinpoo/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/bancokotlinpoo/Pessoa.kt
@@ -1,18 +1,6 @@
 package one.digitalinnovation.bancokotlinpoo
 
-class Pessoa {
-    var name: String = "Neto"
-    var cpf: String = "123.123.123-12"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "name: $name \ncpf: $cpf"
-
-}
-
-fun main() {
-    val neto = Pessoa()
-
-    println(neto.pessoaInfo())
-}
+open class Pessoa (
+    open val nome: String,
+    open val cpf: String
+)
---
+++ src/main/kotlin/one/digitalinnovation/bancokotlinpoo/teste/PessoaTeste.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.bancokotlinpoo.teste
+
+import one.digitalinnovation.bancokotlinpoo.Pessoa
+
+
+fun main() {
+    val neto = Pessoa()
+
+    println(neto.pessoaInfo())
+}
